75d7315819b79bae9fadaada5c9c9ddec1697643
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2023-09-08 17:49:18 +0200

    Request param versioning

--- src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -8,13 +8,21 @@ public class VersioningPersonController {
 
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionPerson() {
-
         return new PersonV1("Bob Marley");
     }
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionPerson() {
+        return new PersonV2(new Name("Bob", "Marley"));
+    }
 
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionPersonRequestParameter() {
+        return new PersonV1("Bob Marley");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionPersonRequestParameter() {
         return new PersonV2(new Name("Bob", "Marley"));
     }
 }
