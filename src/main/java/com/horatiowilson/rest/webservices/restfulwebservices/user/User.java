fc8a8637dc2eef90d32c743dcd55523ca7c82290
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2023-09-12 14:31:00 +0200

    Added static filtering of properties on response example

--- src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.horatiowilson.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +10,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least two characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth date should always be in the past")
+    @JsonProperty("birth_day")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
