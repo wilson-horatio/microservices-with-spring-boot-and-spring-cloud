a5f251be8e8cc5fc8636bded683d0670c6a28310
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2023-09-21 20:20:30 +0200

    Added dynamic filtering of properties on response example

--- src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.horatiowilson.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +15,22 @@ public class FilteringController {
 
     @GetMapping("/filtering")
     public SomeBean filtering() {
-        return new SomeBean("value1", "value2", "value3");
+        return new SomeBean("value1", "value2", "value3", "value4");
+    }
+
+    @GetMapping("/filtering-dynamic")
+    public MappingJacksonValue filteringDynamic() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3", "value4");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
     public List<SomeBean> filteringList() {
-        return Arrays.asList(new SomeBean("value1", "value2", "value3"),
-                new SomeBean("value4", "value5", "value6"));
+        return Arrays.asList(new SomeBean("value1", "value2", "value3", "value4"),
+                new SomeBean("value5", "value6", "value7", "value8"));
     }
 }
--- src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/horatiowilson/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,18 +1,26 @@
 package com.horatiowilson.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
 
     @JsonIgnore
     private String field2;
     private String field3;
+    private String field4;
 
-    public SomeBean(String field1, String field2, String field3) {
+    public SomeBean() {
+
+    }
+
+    public SomeBean(String field1, String field2, String field3, String field4) {
         this.field1 = field1;
         this.field2 = field2;
         this.field3 = field3;
+        this.field4 = field4;
     }
 
     public String getField1() {
@@ -27,12 +35,17 @@ public class SomeBean {
         return field3;
     }
 
+    public String getField4() {
+        return field4;
+    }
+
     @Override
     public String toString() {
         return "SomeBean{" +
                 "field1='" + field1 + '\'' +
                 ", field2='" + field2 + '\'' +
                 ", field3='" + field3 + '\'' +
+                ", field4='" + field4 + '\'' +
                 '}';
     }
 }
